ffffe6e358a79c04ee4cee949e622bb7c7b61f30
Rahul Amaram <rahul.amaram@lithium.com>
2020-09-11 21:22:47 +0530

    Add Pair and Triplet snippets

---
+++ snippets/src/main/java/com/example/snippets/Triplet.java
@@ -0,0 +1,43 @@
+package com.example.snippets;
+
+import java.util.Objects;
+
+public class Triplet<A, B, C> {
+    private final A first;
+    private final B second;
+    private final C third;
+
+    public Triplet(A first, B second, C third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+
+    public A getFirst() {
+        return first;
+    }
+
+    public B getSecond() {
+        return second;
+    }
+
+    public C getThird() {
+        return third;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second, third);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof Triplet))
+            return false;
+        Triplet<?, ?, ?> triplet = (Triplet<?, ?, ?>) o;
+        return Objects.equals(first, triplet.first) && Objects.equals(second, triplet.second)
+                && Objects.equals(third, triplet.third);
+    }
+}
