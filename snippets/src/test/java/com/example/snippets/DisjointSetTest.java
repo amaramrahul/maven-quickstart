d123ef3c565f991f6dc3f9eb49acec7909424633
Rahul Amaram <rahul.amaram@lithium.com>
2020-09-11 19:28:54 +0530

    Add DisjointSet data structure snippet

---
+++ snippets/src/test/java/com/example/snippets/DisjointSetTest.java
@@ -0,0 +1,39 @@
+package com.example.snippets;
+
+import static org.testng.Assert.*;
+
+public class DisjointSetTest {
+
+    @org.testng.annotations.Test
+    public void testUnionBySize() {
+        DisjointSet<Character> ds = new DisjointSet<>();
+        ds.makeSet('a');
+        ds.makeSet('b');
+        ds.makeSet('c');
+        ds.makeSet('d');
+        ds.makeSet('e');
+        ds.makeSet('f');
+        ds.makeSet('g');
+        ds.makeSet('h');
+        ds.makeSet('i');
+        ds.makeSet('j');
+
+        ds.unionBySize('a', 'b');
+        ds.unionBySize('b', 'd');
+        ds.unionBySize('c', 'f');
+        ds.unionBySize('c', 'i');
+        ds.unionBySize('j', 'e');
+        ds.unionBySize('g', 'j');
+
+        assertEquals(ds.find('a').charValue(), 'a');
+        assertEquals(ds.find('b').charValue(), 'a');
+        assertEquals(ds.find('c').charValue(), 'c');
+        assertEquals(ds.find('d').charValue(), 'a');
+        assertEquals(ds.find('e').charValue(), 'j');
+        assertEquals(ds.find('f').charValue(), 'c');
+        assertEquals(ds.find('g').charValue(), 'j');
+        assertEquals(ds.find('h').charValue(), 'h');
+        assertEquals(ds.find('i').charValue(), 'c');
+        assertEquals(ds.find('j').charValue(), 'j');
+    }
+}
